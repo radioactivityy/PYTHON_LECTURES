9dfdc71c84b765a163bef1afccb08f6c27976a30
Gabby <gulcindenirkiran@gmail.com>
2023-12-07 19:15:29 +0100

    first commit

---
+++ homework.py
@@ -0,0 +1,19 @@
+# addition(3, 2) ➞ 5
+# addition(-3, -6) ➞ -9
+# addition(7, 3) ➞ 10
+
+
+# Function to add two numbers 
+def addition(num1,num2):
+    result = num1 + num2 
+    return result
+
+# Testing the function
+result1 = addition(3,2)
+result2 = addition(-3,-6)
+result3 = addition(7,3)
+
+#Printing the results
+print("Result 1:", result1) # 5 
+print("Result 2:", result2) # -9
+print("Result 3:", result3) # 10
